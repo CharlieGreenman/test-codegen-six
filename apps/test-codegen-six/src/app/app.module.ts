5c50e278be02f19fffa3d92b6790444bc045886d
Charlie Greenman <charlie@razroo.com>
2023-08-27 16:06:58 -0700

    Revert "Merge pull request #2 from CharlieGreenman/custom-material-design-recipe"
    This reverts commit a34de94d4948a0eb768a5280f292f463b9d79180, reversing
    changes made to a2e94047e5f75e1e4587bac98706d5e652a123ef.

--- apps/test-codegen-six/src/app/app.module.ts
+++ apps/test-codegen-six/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
